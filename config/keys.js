7028cecc1e241ff4f585be3f360d9181bc9846fd
pweed <godon20078.gk@gmail.com>
2019-04-12 18:41:34 +0000

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Godwin:admin@cluster0-bngko.mongodb.net/test?retryWrites=true',
+  pusherAppId: '758040',
+  pusherKey: '98ba030261f0d0b0e4f5',
+  pusherSecret: '02e10e2e6c9d2477d721',
+  pusherCluster: 'eu',
+  pusherEncrypted: true
+};
